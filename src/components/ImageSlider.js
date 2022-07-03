1d3b6ef63aea697f07a861e286c6dcdf630ea8bb
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-03 23:54:33 +0300

    style : next image  and previous image buttons

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 const slideStyles = {
   width: "100%",
   height: "100%",
@@ -8,27 +9,7 @@ const slideStyles = {
   backgroundPosition: "center",
 };
 
-const rightArrowStyles = {
-  position: "absolute",
-  top: "50%",
-  transform: "translate(0, -50%)",
-  right: "32px",
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-};
 
-const leftArrowStyles = {
-  position: "absolute",
-  top: "50%",
-  transform: "translate(0, -50%)",
-  left: "32px",
-  fontSize: "45px",
-  color: "#fff",
-  zIndex: 1,
-  cursor: "pointer",
-};
 
 const sliderStyles = {
   position: "relative",
@@ -40,10 +21,13 @@ const sliderStyles = {
 const dotStyle = {
   margin: "0 3px",
   cursor: "pointer",
-  fontSize: "20px",
+  fontSize: "10px",
+  color :'white',
 };
 
 const ImageSlider = ({ slides }) => {
+const [visible,setVisible]=useState(false);
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0;
@@ -64,14 +48,14 @@ const ImageSlider = ({ slides }) => {
   };
 
   return (
-    <div style={sliderStyles}>
+    <div style={sliderStyles}  onMouseOver={() =>   setVisible(true)} onMouseLeave={() =>   setVisible(false)}>
       <div>
-        <div onClick={goToPrevious} style={leftArrowStyles}>
+        {visible && <button onClick={goToPrevious} className="leftArrowStyles">
           ❰
-        </div>
-        <div onClick={goToNext} style={rightArrowStyles}>
+        </button>}
+        {visible &&<button onClick={goToNext} className="rightArrowStyles">
           ❱
-        </div>
+        </button>}
         
       </div>
       <div style={slideStylesWidthBackground}><div className="bottom"><div className="dotsContainerStyles">
