7e9b5c3afe5ea31d180b22ff034bdc41ed9747ad
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-03 18:19:58 +0300

    build : image slider component (to view images as a slider)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import posts from './data.json';
+import ImageSlider from './components/ImageSlider'
 
 function App() {
   const data = posts["data"];
+  const images=data[0]['images'];
+
   return (
-    <div className="App">
-      <img  src={data[0]['images'][0]['url']} />
+    // <div className="App">
+    //   <img  src={data[0]['images'][0]['url']} />
+    //   <img  src={data[0]['images'][1]['url']} />
+    //   <img  src={data[0]['images'][2]['url']} />
+    //   <img  src={data[0]['images'][3]['url']} />
+    //   <img  src={data[0]['images'][4]['url']} />
       
-    </div>
+    // </div>
+    <><ImageSlider slides={slides} /></>
   );
 }
 
