4c55c663820b57950987a3d6913320b001ecf3ea
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-04 09:55:27 +0300

    style : the button in the middle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import logo from "../../src/Airbnb-logo.jpg"
+import magnifier from "../../src/magnifier.png"
 
 const Navbar = () => {
 
@@ -11,9 +12,11 @@ const Navbar = () => {
         <img src={logo}/>
 
         <button className="mid">
-            <a>Anywhere |</a>
-            <a>Any week |</a>
-            <a>Add week</a>
+            <a>Anywhere </a>
+            <p>|</p>
+            <a>Any week </a>
+            <p>|</p>
+            <a className="last">Add guests</a>
         </button>
         
         
