f8006c4daa9f118903624e327605fae5cd4d421e
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-07-04 09:30:10 +0300

    feat : logo in the navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import logo from "../../src/Airbnb-logo.jpg"
+
+const Navbar = () => {
+
+
+
+    return(<><div className="header-container">
+        
+        <img src={logo}/>
+
+        
+        
+        
+        
+        </div></>);
+}
+export default Navbar;
